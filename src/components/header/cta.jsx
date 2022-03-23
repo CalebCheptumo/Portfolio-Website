5a8f8dc74115963ddf70f8ec9ccdcd0194151f4d
calebcheptumo <ckibusia@gmail.com>
2022-03-23 15:56:15 +0300

    Header Component

---
+++ src/components/header/cta.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const cta = () => {
+    return (
+        <div  className = 'cta'>
+            <a href="#about" className='btn btn-primary'>About Me</a>
+            <a href="#portfolio" className='btn'>Projects</a>
+        </div>
+    )
+}
+
+export default cta
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import './header.css'
+import CTA from './cta'
+import ME from '../../assets/me.png'
 
 const header = () => {
     return (
-        <div>
-            Header
-        </div>
+       <header>
+           <div className = "container header__container">
+               <h5>Hello I'm</h5>
+               <h1>Caleb Cheptumo</h1>
+               <h5 className = "text-light">Web developer</h5>
+               <CTA />
+
+               <div className="me">
+                   <img src= {ME} alt="Caleb cheptumo" />
+               </div>
+           </div>
+
+
+       </header>
     )
 }
 
