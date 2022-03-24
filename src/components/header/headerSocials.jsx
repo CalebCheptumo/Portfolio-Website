e248f25ceb2f3c6b6b07ba9e2c4eda0e4d84e551
calebcheptumo <ckibusia@gmail.com>
2022-03-25 00:33:44 +0300

    add Header social components

---
+++ src/components/header/headerSocials.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
+import {BsTwitter} from 'react-icons/bs'
+const headerSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href="https://linkedin.com" target='_blank'><BsLinkedin/> </a>
+        <a href="https://github.com" target= '_blank'><BsGithub/></a>
+        <a href="https://twitter.com" target= '_blank'><BsTwitter/></a>
+    </div>
+  )
+}
+
+export default headerSocials
