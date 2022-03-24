51410d6df20b71c019962e5e9d63ab5e975a99e7
calebcheptumo <ckibusia@gmail.com>
2022-03-25 02:30:36 +0300

    managing nav states using useState hook

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,18 +4,20 @@ import {FaHome} from 'react-icons/fa'
 import {FaUser} from 'react-icons/fa'
 import {AiOutlineFundProjectionScreen} from 'react-icons/ai'
 import {BiMessageRounded} from 'react-icons/bi'
+import { useState } from 'react'
 
 
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#" className='active'><FaHome/></a>
-      <a href="#about"><FaUser/></a>
-      <a href="#portfolio"><AiOutlineFundProjectionScreen/></a>
-      <a href="#contact"><BiMessageRounded/></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav  === '#' ? 'active' : ''} ><FaHome/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav  === '#about' ? 'active' : ''}><FaUser/></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav  === '#portfolio' ? 'active' : ''}><AiOutlineFundProjectionScreen/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav  === '#contact' ? 'active' : ''}><BiMessageRounded/></a>
     </nav>
-  )
+  ) 
 }
 
 export default Nav
