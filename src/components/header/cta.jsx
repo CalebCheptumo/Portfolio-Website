95a4e68cf578921d6f5c0294b0f9a415f42042d5
calebcheptumo <ckibusia@gmail.com>
2022-03-25 04:49:28 +0300

    portfolio component JSX

--- src/components/header/cta.jsx
+++ src/components/header/cta.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import './cta.css'
 import {AiOutlineUser} from 'react-icons/ai'
 import {BsFillEyeFill} from 'react-icons/bs'
 const cta = () => {
     return (
         <div  className = 'cta'>
-            <a href="#about" className='btn btn-primary'>About Me<AiOutlineUser/></a>
-            <a href="#portfolio" className='btn'>Projects<BsFillEyeFill/></a>
+            <a href="#about" className='btn btn-primary'>About Me<AiOutlineUser className='cta-icon'/></a>
+            <a href="#portfolio" className='btn'>Projects<BsFillEyeFill className='cta-icon'/></a>
         </div>
     )
 }
--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/p1.png'
+import IMG2 from '../../assets/p2.png'
+import IMG3 from '../../assets/p3.png'
 
 const portfolio = () => {
     return (
-        <section id='portfolio'>portfolio</section>
+        <section id='portfolio'>
+            <h5>Featured projects:</h5>
+
+            <div className='container portfolio__container'>
+                 <article className='portfolio__item'>
+                     <div className="portfolio__item-image">
+                         <img src={IMG1} alt="" />
+                     </div>
+                     <h3>Tracking Tool</h3>
+                     <a href="" className='btn btn-primary' target='_blank'>View Live</a>
+                     <a href="https://github.com" className='btn' target='_blank'>Github Repo</a>
+                     
+                 </article>
+                 <article className='portfolio__item'>
+                     <div className="portfolio__item-image">
+                         <img src={IMG2} alt="" />
+                     </div>
+                     <h3>UI KIT</h3>
+                     <a href="" className='btn btn-primary' target='_blank'>View Live</a>
+                     <a href="https://github.com" className='btn' target='_blank'>Github Repo</a>
+                     
+                 </article>
+                 <article className='portfolio__item'>
+                     <div className="portfolio__item-image">
+                         <img src={IMG3} alt="" />
+                     </div>
+                     <h3>ANALYSIS TOOL</h3>
+                     <a href="" className='btn btn-primary' target='_blank'>View Live</a>
+                     <a href="https://github.com" className='btn' target='_blank'>Github Repo</a>
+                     
+                 </article>
+            </div>
+        
+           </section>
     )
 }
 
