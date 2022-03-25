29463ed2c62ec4ed3fb4a6fe1df5343361158a52
calebcheptumo <ckibusia@gmail.com>
2022-03-25 14:29:09 +0300

    footer component JSX

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsTwitter} from 'react-icons/bs'
+import {BsInstagram} from 'react-icons/bs'
+
+
 
 
 const footer = () => {
     return (
-        <div>
-           Footer
-        </div>
+        <footer>
+            <ul className='permalinks'>
+                <li><a href="#"></a>Home</li>
+                <li><a href="#about"></a>About</li>
+                <li><a href="portfolio"></a>Portfolio</li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+            <div className="footer__socials">
+                <a href="linkedin"><BsLinkedin/></a>
+                <a href="twitter"><BsTwitter/></a>
+                <a href="instagram"><BsInstagram/></a>
+              
+            </div>
+            <div className="footer__copyright">
+                <small>&copy; Caleb Cheptumo.</small>
+            </div>
+        </footer>
     )
 }
 
