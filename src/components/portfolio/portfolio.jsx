b505c284f5185e880f9b10e6bc0e56ccd958330c
calebcheptumo <ckibusia@gmail.com>
2022-03-25 05:33:27 +0300

    generating portfolio Items from Array using map method

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -4,46 +4,54 @@ import IMG1 from '../../assets/p1.png'
 import IMG2 from '../../assets/p2.png'
 import IMG3 from '../../assets/p3.png'
 
+
+const data = [
+    {
+        id:1,
+        image:IMG1,
+        title: 'UI KIT',
+        github:'',
+        demo:'',
+    },
+    {
+        id:2,
+        image:IMG2,
+        title: 'Analysis tool',
+        github:'',
+        demo:'',
+    },
+    {
+        id:3,
+        image:IMG3,
+        title: 'Tracking Tool',
+        github:'',
+        demo:'',
+    },
+]
+
 const portfolio = () => {
     return (
         <section id='portfolio'>
             <h5>Featured projects:</h5>
 
             <div className='container portfolio__container'>
+          {
+            data.map(({id,image,title,github,demo}) => {
+                return(
                  <article className='portfolio__item'>
                      <div className="portfolio__item-image">
-                         <img src={IMG1} alt="" />
+                         <img src={image} alt={title} />
                      </div>
-                     <h3>Tracking Tool</h3>
-                     <p>Tool to report expected and unexpected errors on website</p>
+                     <h3>{title}</h3>
                      <div className="portfolio__item-cta">
-                     <a href="" className='btn btn-primary' target='_blank'>View Live</a>
-                     <a href="https://github.com" className='btn' target='_blank'>Github Repo</a>
+                     <a href={demo} className='btn btn-primary' target='_blank'>View Live</a>
+                     <a href={github} className='btn' target='_blank'>Github Repo</a>
                      </div>
                      
                  </article>
-                 <article className='portfolio__item'>
-                     <div className="portfolio__item-image">
-                         <img src={IMG2} alt="" />
-                     </div>
-                     <h3>UI KIT</h3>
-                     <div className="portfolio__item-cta">
-                     <a href="" className='btn btn-primary' target='_blank'>View Live</a>
-                     <a href="https://github.com" className='btn' target='_blank'>Github Repo</a>
-                     </div>
-                     
-                 </article>
-                 <article className='portfolio__item'>
-                     <div className="portfolio__item-image">
-                         <img src={IMG3} alt="" />
-                     </div>
-                     <h3>ANALYSIS TOOL</h3>
-                     <div className="portfolio__item-cta">
-                     <a href="" className='btn btn-primary' target='_blank'>View Live</a>
-                     <a href="https://github.com" className='btn' target='_blank'>Github Repo</a>
-                     </div>
-                     
-                 </article>
+                )
+            })
+        }
             </div>
         
            </section>
